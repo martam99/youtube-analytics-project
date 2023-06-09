2286ac1167b36618b42356129fd75e5ccf387afa
Mariam Tamrazyan <mart76901@gmail.com>
2023-06-09 23:40:57 +0400

    added try/except for class Video

--- src/video.py
+++ src/video.py
@@ -1,22 +1,32 @@
 from src.mixin_api import APIMixin
+from pprint import pprint
 
 
 class Video(APIMixin):
     def __init__(self, video_id: str) -> None:
-        self.video_id = video_id
-        self.video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                               id=video_id
-                                                               ).execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.video_url: str = self.video_response['items'][0]['snippet']['thumbnails']['default']['url']
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            self.video_response = self.get_service().videos().list(
+                                                                   part='snippet,statistics,contentDetails,topicDetails',
+                                                                   id=video_id
+                                                                   ).execute()
+            self.title: str = self.video_response['items'][0]['snippet']['localized']["title"]
+            self.video_url: str = self.video_response['items'][0]['snippet']['thumbnails']['default']['url']
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_id = video_id
+            self.title = None
+            self.video_url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
+
